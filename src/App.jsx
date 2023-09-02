36b19ed4beaddca8ec6d48b41f4274b622d27859
Kush <kush.73888@gmail.com>
2023-09-03 01:23:30 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Hero from './components/Hero';
 import Demo from './components/Demo';
-
 import './App.css';
+
 const App = () => {
   return (
     <main>
       <div className='main'>
-          <div className='gradient'>
-          </div>  
+          <div className='gradient' />
       </div>
 
       <div className='app'>
